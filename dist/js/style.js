ca6c2efcd976aacf4df380b63acfec00a7dfce28
Louis <neighborhood0830@gmail.com>
2017-02-07 15:03:58 +0800

    edit main.js

--- dist/js/style.js
+++ dist/js/style.js
@@ -1,1 +1,191 @@
-var bluetech=function(e){function _(n){if(r[n])return r[n].exports;var c=r[n]={exports:{},id:n,loaded:!1};return e[n].call(c.exports,c,c.exports,_),c.loaded=!0,c.exports}var r={};return _.m=e,_.c=r,_.p="",_(0)}(function(e){for(var _ in e)if(Object.prototype.hasOwnProperty.call(e,_))switch(typeof e[_]){case"function":break;case"object":e[_]=function(_){var r=_.slice(1),n=e[_[0]];return function(e,_,c){n.apply(this,[e,_,c].concat(r))}}(e[_]);break;default:e[_]=e[e[_]]}return e}({0:function(module,exports,__webpack_require__){eval("\"use strict\";\n\n__webpack_require__(117);\n__webpack_require__(124);\n__webpack_require__(132);\n\n__webpack_require__(126);\n__webpack_require__(115);\n__webpack_require__(125);\n__webpack_require__(135);\n__webpack_require__(138);\n__webpack_require__(136);\n__webpack_require__(131);\n__webpack_require__(127);\n__webpack_require__(128);\n__webpack_require__(130);\n__webpack_require__(118);\n__webpack_require__(134);\n__webpack_require__(133);\n//require(\"../../vendors/pnotify/dist/pnotify.nonblock.css\");\n__webpack_require__(119);\n__webpack_require__(120);\n__webpack_require__(121);\n__webpack_require__(122);\n__webpack_require__(123);\n__webpack_require__(116);\n__webpack_require__(137);\n__webpack_require__(113);\n//  var layer= require('../../vendors/layer/build/layer.js');\n\n// require('../../vendors/ng-table/dist/ng-table.css');\n\n\n//left mune scrollbar\n__webpack_require__(129);\n\n__webpack_require__(114);\n\n//////////////////\n// WEBPACK FOOTER\n// ./src/config/style.js\n// module id = 0\n// module chunks = 1\n//# sourceURL=webpack:///./src/config/style.js?")},113:function(module,exports){eval("// removed by extract-text-webpack-plugin\n\n//////////////////\n// WEBPACK FOOTER\n// ./src/css/layer/default/layer.css\n// module id = 113\n// module chunks = 1\n//# sourceURL=webpack:///./src/css/layer/default/layer.css?")},114:113,115:113,116:113,117:113,118:113,119:113,120:113,121:113,122:113,123:113,124:113,125:113,126:113,127:113,128:113,129:113,130:113,131:113,132:113,133:113,134:113,135:113,136:113,137:113,138:113}));
+var bluetech=/******/
+function(e){/******/
+/******/
+// The require function
+/******/
+function _(n){/******/
+/******/
+// Check if module is in cache
+/******/
+if(r[n])/******/
+return r[n].exports;/******/
+/******/
+// Create a new module (and put it into the cache)
+/******/
+var c=r[n]={/******/
+exports:{},/******/
+id:n,/******/
+loaded:!1};/******/
+/******/
+// Return the exports of the module
+/******/
+/******/
+/******/
+// Execute the module function
+/******/
+/******/
+/******/
+// Flag the module as loaded
+/******/
+return e[n].call(c.exports,c,c.exports,_),c.loaded=!0,c.exports}// webpackBootstrap
+/******/
+// The module cache
+/******/
+var r={};/******/
+/******/
+// Load entry module and return exports
+/******/
+/******/
+/******/
+/******/
+// expose the modules object (__webpack_modules__)
+/******/
+/******/
+/******/
+// expose the module cache
+/******/
+/******/
+/******/
+// __webpack_public_path__
+/******/
+return _.m=e,_.c=r,_.p="",_(0)}(function(e){
+// Check all modules for deduplicated modules
+for(var _ in e)if(Object.prototype.hasOwnProperty.call(e,_))switch(typeof e[_]){case"function":break;case"object":
+// Module can be created from a template
+e[_]=function(_){var r=_.slice(1),n=e[_[0]];return function(e,_,c){n.apply(this,[e,_,c].concat(r))}}(e[_]);break;default:
+// Module is a copy of another module
+e[_]=e[e[_]]}return e}([/* 0 */
+/***/
+function(module,exports,__webpack_require__){eval("\"use strict\";\n\n__webpack_require__(110);\n__webpack_require__(117);\n__webpack_require__(125);\n\n__webpack_require__(119);\n__webpack_require__(108);\n__webpack_require__(118);\n__webpack_require__(128);\n__webpack_require__(131);\n__webpack_require__(129);\n__webpack_require__(124);\n__webpack_require__(120);\n__webpack_require__(121);\n__webpack_require__(123);\n__webpack_require__(111);\n__webpack_require__(127);\n__webpack_require__(126);\n//require(\"../../vendors/pnotify/dist/pnotify.nonblock.css\");\n__webpack_require__(112);\n__webpack_require__(113);\n__webpack_require__(114);\n__webpack_require__(115);\n__webpack_require__(116);\n__webpack_require__(109);\n__webpack_require__(130);\n__webpack_require__(106);\n//  var layer= require('../../vendors/layer/build/layer.js');\n\n// require('../../vendors/ng-table/dist/ng-table.css');\n\n\n//left mune scrollbar\n__webpack_require__(122);\n\n__webpack_require__(107);\n\n//////////////////\n// WEBPACK FOOTER\n// ./src/config/style.js\n// module id = 0\n// module chunks = 1\n//# sourceURL=webpack:///./src/config/style.js?")},/* 1 */
+,/* 2 */
+,/* 3 */
+,/* 4 */
+,/* 5 */
+,/* 6 */
+,/* 7 */
+,/* 8 */
+,/* 9 */
+,/* 10 */
+,/* 11 */
+,/* 12 */
+,/* 13 */
+,/* 14 */
+,/* 15 */
+,/* 16 */
+,/* 17 */
+,/* 18 */
+,/* 19 */
+,/* 20 */
+,/* 21 */
+,/* 22 */
+,/* 23 */
+,/* 24 */
+,/* 25 */
+,/* 26 */
+,/* 27 */
+,/* 28 */
+,/* 29 */
+,/* 30 */
+,/* 31 */
+,/* 32 */
+,/* 33 */
+,/* 34 */
+,/* 35 */
+,/* 36 */
+,/* 37 */
+,/* 38 */
+,/* 39 */
+,/* 40 */
+,/* 41 */
+,/* 42 */
+,/* 43 */
+,/* 44 */
+,/* 45 */
+,/* 46 */
+,/* 47 */
+,/* 48 */
+,/* 49 */
+,/* 50 */
+,/* 51 */
+,/* 52 */
+,/* 53 */
+,/* 54 */
+,/* 55 */
+,/* 56 */
+,/* 57 */
+,/* 58 */
+,/* 59 */
+,/* 60 */
+,/* 61 */
+,/* 62 */
+,/* 63 */
+,/* 64 */
+,/* 65 */
+,/* 66 */
+,/* 67 */
+,/* 68 */
+,/* 69 */
+,/* 70 */
+,/* 71 */
+,/* 72 */
+,/* 73 */
+,/* 74 */
+,/* 75 */
+,/* 76 */
+,/* 77 */
+,/* 78 */
+,/* 79 */
+,/* 80 */
+,/* 81 */
+,/* 82 */
+,/* 83 */
+,/* 84 */
+,/* 85 */
+,/* 86 */
+,/* 87 */
+,/* 88 */
+,/* 89 */
+,/* 90 */
+,/* 91 */
+,/* 92 */
+,/* 93 */
+,/* 94 */
+,/* 95 */
+,/* 96 */
+,/* 97 */
+,/* 98 */
+,/* 99 */
+,/* 100 */
+,/* 101 */
+,/* 102 */
+,/* 103 */
+,/* 104 */
+,/* 105 */
+,/* 106 */
+/***/
+function(module,exports){eval("// removed by extract-text-webpack-plugin\n\n//////////////////\n// WEBPACK FOOTER\n// ./src/css/layer/default/layer.css\n// module id = 106\n// module chunks = 1\n//# sourceURL=webpack:///./src/css/layer/default/layer.css?")},/* 107 */
+106,/* 108 */
+106,/* 109 */
+106,/* 110 */
+106,/* 111 */
+106,/* 112 */
+106,/* 113 */
+106,/* 114 */
+106,/* 115 */
+106,/* 116 */
+106,/* 117 */
+106,/* 118 */
+106,/* 119 */
+106,/* 120 */
+106,/* 121 */
+106,/* 122 */
+106,/* 123 */
+106,/* 124 */
+106,/* 125 */
+106,/* 126 */
+106,/* 127 */
+106,/* 128 */
+106,/* 129 */
+106,/* 130 */
+106,/* 131 */
+106]));
--- src/config/main.js
+++ src/config/main.js
@@ -1,6 +1,7 @@
 define(['jquery', 'angular', 'moment', 'PNotify', 'jquery-mousewheel', 'datatables.net'], function (jquery, angular, moment, PNotify, jqueryMousewheel, datatables) {
     'use strict';
-
+    console.log(angular);
+    global.angular = angular;
     var $ = jquery;
     global.jQuery = $;
     global.$ = $;
@@ -16,74 +17,29 @@ define(['jquery', 'angular', 'moment', 'PNotify', 'jquery-mousewheel', 'datatabl
     require("../../vendors/nprogress/nprogress.js");
     require("../../vendors/bootstrap-progressbar/bootstrap-progressbar.min.js");
 
-
-
     //left mune scrollbar
     require("../../vendors/jquery-mousewheel/jquery.mousewheel.min.js");
     require("../../vendors/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js");
-
-
     global.echarts = require("../../vendors/echarts/dist/echarts.min.js");
-
-
     require("../../vendors/iCheck/icheck.min.js");
-
-
     require("../../vendors/skycons/skycons.js");
-
-
     require("../../vendors/bootstrap-daterangepicker/daterangepicker.js");
-
-
     require('../../vendors/blockUI/jquery.blockUI.js');
-
-
     require('../../vendors/select2/dist/js/select2.full.min.js');
-
-
     require('../../vendors/jquery.tagsinput/src/jquery.tagsinput.js');
-
-
     require('../../vendors/parsleyjs/dist/parsley.min.js');
-
-
     require('../../vendors/datatables.net/js/jquery.dataTables.min.js');
-
-
     require("../../vendors/datatables.net-bs/js/dataTables.bootstrap.min.js");
-
-
     require('../../vendors/ion.rangeSlider/js/ion.rangeSlider.min.js');
-
-
     require('../../vendors/jquery-slimscroll/jquery.slimscroll.min.js');
-
-
     require('../../vendors/pnotify/dist/pnotify.js');
-
-
     require('../../vendors/sweetalert2/dist/sweetalert2.js');
-
-
     require('../../vendors/bootstrap-switch/dist/js/bootstrap-switch.min.js');
-
-
-    // 這一個有問題
     require('../../vendors/mjolnic-bootstrap-colorpicker/dist/js/bootstrap-colorpicker.min.js');
-
     require('../../vendors/jquery.inputmask/dist/min/jquery.inputmask.bundle.min.js');
-
-
-
     require('../../vendors/jquery-knob/dist/jquery.knob.min.js');
-
-
     require('../../vendors/cropper/dist/cropper.min.js');
-
-
     require("../../vendors/promise-finally/Main.js");
-
-
     var layer = require('../../vendors/layer/build/layer.js');
     layer.config({
         path: false
@@ -97,7 +53,7 @@ define(['jquery', 'angular', 'moment', 'PNotify', 'jquery-mousewheel', 'datatabl
     require("../../vendors/angular-route/angular-route.min.js");
     require("../../vendors/angular-datatables/dist/angular-datatables.min.js");
     angular.module('bluetech', ['ngRoute', 'datatables']);
+    var app = angular.module('bluetech', []);
 
-
-    return angular;
+    return app;
 });
