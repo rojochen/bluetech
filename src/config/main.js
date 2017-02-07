6cc89d62a95f5316d12df519fcc4d6eb208321db
Louis <neighborhood0830@gmail.com>
2017-02-07 10:43:27 +0800

    webpack fix

--- src/config/main.js
+++ src/config/main.js
@@ -1,4 +1,4 @@
-define(['jquery', 'angular', 'moment', 'PNotify'], function (jquery, angular, moment, PNotify) {
+define(['jquery', 'angular', 'moment', 'PNotify', 'jquery-mousewheel', 'datatables.net'], function (jquery, angular, moment, PNotify, jqueryMousewheel, datatables) {
     'use strict';
 
     var $ = jquery;
@@ -19,90 +19,83 @@ define(['jquery', 'angular', 'moment', 'PNotify'], function (jquery, angular, mo
 
 
     //left mune scrollbar
-    require('../../vendors/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js');
-    // require("../../node_modules/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js");
+    require("../../vendors/jquery-mousewheel/jquery.mousewheel.min.js");
+    require("../../vendors/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js");
+
 
     global.echarts = require("../../vendors/echarts/dist/echarts.min.js");
-    // global.echarts = require("../../node_modules/echarts/dist/echarts.min.js");
+
 
     require("../../vendors/iCheck/icheck.min.js");
-    // require("../../node_modules/icheck/icheck.min.js");
+
 
     require("../../vendors/skycons/skycons.js");
-    // require("../../node_modules/skycons/skycons.js");
+
 
     require("../../vendors/bootstrap-daterangepicker/daterangepicker.js");
-    // require("../../node_modules/bootstrap-daterangepicker/daterangepicker.js");
+
 
     require('../../vendors/blockUI/jquery.blockUI.js');
-    // require("../../node_modules/block-ui/jquery.blockUI.js");
+
 
     require('../../vendors/select2/dist/js/select2.full.min.js');
-    // require("../../node_modules/select2/dist/js/select2.full.min.js");
+
 
     require('../../vendors/jquery.tagsinput/src/jquery.tagsinput.js');
-    // require("../../node_modules/jquery-tags-input/dist/jquery.tagsinput.min.js");
+
 
     require('../../vendors/parsleyjs/dist/parsley.min.js');
-    // require("../../node_modules/parsleyjs/dist/parsley.min.js");
+
 
     require('../../vendors/datatables.net/js/jquery.dataTables.min.js');
-    // require("../../node_modules/datatables.net/js/jquery.dataTables.js");
 
-    require('../../vendors/datatables.net-bs/js/dataTables.bootstrap.min.js');
-    // require("../../node_modules/datatables.net-bs/js/dataTables.bootstrap.js");
+
+    require("../../vendors/datatables.net-bs/js/dataTables.bootstrap.min.js");
+
 
     require('../../vendors/ion.rangeSlider/js/ion.rangeSlider.min.js');
-    // require("../../node_modules/ion-rangeslider/js/ion.rangeSlider.min.js");
+
 
     require('../../vendors/jquery-slimscroll/jquery.slimscroll.min.js');
-    // require("../../node_modules/jquery-slimscroll/jquery.slimscroll.min.js");
+
 
     require('../../vendors/pnotify/dist/pnotify.js');
-    // require("../../node_modules/pnotify/dist/pnotify.js");
+
 
     require('../../vendors/sweetalert2/dist/sweetalert2.js');
-    // require("../../node_modules/sweetalert2/dist/sweetalert2.min.js");
+
 
     require('../../vendors/bootstrap-switch/dist/js/bootstrap-switch.min.js');
-    // require("../../node_modules/bootstrap-switch/dist/js/bootstrap-switch.min.js");
+
 
     // 這一個有問題
     require('../../vendors/mjolnic-bootstrap-colorpicker/dist/js/bootstrap-colorpicker.min.js');
 
     require('../../vendors/jquery.inputmask/dist/min/jquery.inputmask.bundle.min.js');
-    // require("../../node_modules/jquery.inputmask/dist/jquery.inputmask.bundle.js");
+
 
 
     require('../../vendors/jquery-knob/dist/jquery.knob.min.js');
-    // require("../../node_modules/jquery-knob/dist/jquery.knob.min.js");
+
 
     require('../../vendors/cropper/dist/cropper.min.js');
-    // require("../../node_modules/cropper/dist/cropper.min.js");
 
-    // require('promise-finally');
-    require("../../node_modules/promise-finally/dist/promise-finally.js");
+
+    require("../../vendors/promise-finally/Main.js");
+
 
     var layer = require('../../vendors/layer/build/layer.js');
     layer.config({
         path: false
     });
-    // var layer = require("../../node_modules/layer/layer.min.js");
-    // layer.config({
-    //     path: false
-    // });
+
     // util
     require("../config/util.js");
 
 
     // angular
     require("../../vendors/angular-route/angular-route.min.js");
-    // require("../../node_modules/angular-route/angular-route.min.js");
-
     require("../../vendors/angular-datatables/dist/angular-datatables.min.js");
-    // require("../../node_modules/angular-datatables/dist/angular-datatables.min.js");
-
-    // angular.module('pokemon', ['ngRoute', 'datatables']);
     angular.module('bluetech', ['ngRoute', 'datatables']);
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ module.exports = {
     cache: true,
     devtool: 'eval',
     entry: {
-        bluetech: "src/config/main.js",
-        style: "src/config/style.js"
+        bluetech: "./src/config/main.js",
+        style: "./src/config/style.js"
     },
     output: {
         path: "./dist/js",
@@ -17,42 +17,42 @@ module.exports = {
         alias: {
             'angular': 'angular/angular',
             'jquery': 'jquery/dist/jquery.min',
-            'moment': 'moment/src/moment',
-            'PNotify': 'pnotify/dist/pnotify'
+            'moment': 'moment/moment',
+            'PNotify': 'pnotify/dist/pnotify',
+            'jquery-mousewheel': 'jquery-mousewheel/jquery.mousewheel.min',
+            "datatables.net": 'datatables.net-bs/js/dataTables.bootstrap',
         },
         extensions: ['', '.js']
     },
     module: {
         loaders: [{
-            test: /\.js$/,
-            exclude: /(node_modules|bower_components)/,
-            loader: 'babel-loader',
-            query: {
-                presets: ['es2015'],
-                plugins: ['transform-runtime']
+                test: /\.js$/,
+                exclude: /(node_modules|vendors)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            },
+            {
+                test: /\.(png|gif)$/,
+                loader: 'url-loader?limit=100000'
+            }, {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css?minimize!sass")
+            }, {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract("style", "css?minimize!sass")
+            }, {
+                test: /\.(jpg|woff|svg|ttf|eot)([\?]?.*)$/,
+                loader: "file-loader?name=../css/img/[name].[ext]"
+            }, {
+                test: /[\/\\]angular\.js$/,
+                loader: "exports?window.angular"
+            }, {
+                test: /pnotify.*\.js$/,
+                loader: "imports?define=>false,global=>window"
             }
-        }, {
-            test: /\.(png|gif)$/,
-            loader: 'url-loader?limit=100000'
-        }, {
-            test: /\.css$/,
-            loader: ExtractTextPlugin.extract("style-loader", "css?minimize!sass")
-        }, {
-            test: /\.scss$/,
-            loader: ExtractTextPlugin.extract("style", "css?minimize!sass")
-        }, {
-            test: /\.(jpg|woff|svg|ttf|eot)([\?]?.*)$/,
-            loader: "file-loader?name=../css/img/[name].[ext]"
-        }, {
-            test: /[\/\\]angular\.js$/,
-            loader: "exports?window.angular"
-        }, {
-            test: /pnotify.*\.js$/,
-            loader: "imports?define=>false,global=>window"
-        }, {
-            test: /\.(eot|svg|ttf|woff|woff2)$/,
-            loader: 'file?name=../css/fonts/[name].[ext]'
-        }]
+        ]
     },
     plugins: [
         new webpack.ProvidePlugin({
